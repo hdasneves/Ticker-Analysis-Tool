95a00ce207b96567b6592e72060ab06c604ace8b
Hugo Das Neves <152240580+mortleb@users.noreply.github.com>
2024-12-15 16:47:13 +0100

    Update fun.py

--- fun.py
+++ fun.py
@@ -1,12 +1,6 @@
 import subprocess
 import pandas as pd
 
-def remake_file():
-    yah = ["pyside6-uic", "yahoo.ui", "-o", "yahoo.py"]
-    tab = ["pyside6-uic", "table.ui", "-o", "table.py"]
-    subprocess.run(yah, check=True)
-    subprocess.run(tab, check=True)
-
 def get_ratio(text_1, data_1, text_2, data_2):
     ratio_data = pd.merge(left = data_1, right = data_2, on = "Date", suffixes = (f"_{text_1}", f"_{text_2}"))
     ratio_data["Close"] = ratio_data[f"Close_{text_1}"] / ratio_data[f"Close_{text_2}"] 
